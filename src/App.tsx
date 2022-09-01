a76ace34f9fda511ef3109084937bb930b993d25
GSV78 <gradov78@gmail.com>
2022-09-01 18:21:43 +0300

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,29 @@ import Servises from './Components/Servises/Servises';
 import Content from './Components/Content/Content';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Conditioners from './Components/Conditioners/Conditioners'
-import Cameras from './Components/Cameras/Cameras'
-import Security from './Components/Security/Security'
-import Network from './Components/Network/Network'
-import Smarthome from './Components/Smarthome/Smarthome'
-import Install from './Components/Install/Install'
-import Gsm from './Components/Gsm/Gsm'
+import Conditioners from './Components/Conditioners/Conditioners';
+import Cameras from './Components/Cameras/Cameras';
+import Security from './Components/Security/Security';
+import Network from './Components/Network/Network';
+import Smarthome from './Components/Smarthome/Smarthome';
+import Install from './Components/Install/Install';
+import Gsm from './Components/Gsm/Gsm';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path={'/*'} element={<div>
-          <Servises />
-          <Content />
-        </div>} />
+        <Route
+          path={'/*'}
+          element={
+            <div>
+              <Servises />
+              <Content />
+            </div>
+          }
+        />
         <Route path={'/conditioners/*'} element={<Conditioners />} />
         <Route path={'/cameras/*'} element={<Cameras />} />
         <Route path={'/security/*'} element={<Security />} />
@@ -29,6 +35,7 @@ function App() {
         <Route path={'/install/*'} element={<Install />} />
         <Route path={'/gsm/*'} element={<Gsm />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
